b2ff4d8aa02d585d99472398a9bdf82f99f2fca9
Ona <ona.siscart@estudiantat.upc.edu>
2025-10-13 08:27:39 +0200

    extract falta error handling

--- extract.py
+++ extract.py
@@ -1,50 +1,135 @@
 from pathlib import Path
 import psycopg2
 import pandas as pd
+
 # https://pygrametl.org
-from pygrametl.datasources import CSVSource, SQLSource
+# from pygrametl.datasources import CSVSource, SQLSource
 
 
 # Connect to the PostgreSQL source
 path = Path("db_conf.txt")
 if not path.is_file():
-    raise FileNotFoundError(f"Database configuration file '{path.absolute()}' not found.")
+    raise FileNotFoundError(
+        f"Database configuration file '{path.absolute()}' not found."
+    )
 try:
     parameters = {}
     # Read the database configuration from the provided txt file, line by line
-    with open(path, 'r') as f:
+    with open(path, "r") as f:
         lines = f.readlines()
         for line in lines:
-            parameters[line.split('=', 1)[0]] = line.split('=', 1)[1].strip()
+            parameters[line.split("=", 1)[0]] = line.split("=", 1)[1].strip()
     conn = psycopg2.connect(
-        dbname=parameters['dbname'],
-        user=parameters['user'],
-        password=parameters['password'],
-        host=parameters['ip'],
-        port=parameters['port']
+        dbname=parameters["dbname"],
+        user=parameters["user"],
+        password=parameters["password"],
+        host=parameters["ip"],
+        port=parameters["port"],
     )
 except psycopg2.Error as e:
     print(e)
     raise ValueError(f"Unable to connect to the database: {parameters}")
 except Exception as e:
     print(e)
-    raise ValueError(f"Database configuration file '{path.absolute()}' not properly formatted (check file 'db_conf.example.txt'.")
+    raise ValueError(
+        f"Database configuration file '{path.absolute()}' not properly formatted (check file 'db_conf.example.txt'."
+    )
 
 
 # TODO: Implement here all the extracting functions
 
 
+# function to extract all necessary files
+def extract_all(conn: psycopg2.extensions.connection) -> dict[str, pd.DataFrame]:
+    """
+    Prec: connection to DBBDA established
+    Post: returns a dictionary with extracted tables (AIMS_flights, AIMS_maint, AMOS, Maintenance, Aircrafts)
+           as pandas dataframes
+    """
+    extracted_data: dict[str, pd.DataFrame] = {}
+
+    # Extract data from SQL source files
+    extract_SQL_data(extracted_data, conn)
+
+    # paths to csv files
+    csv_maintenance_path = "maintenance_personnel.csv"
+    csv_aircrafts_path = "aircraft-manufacturerinfo-lookup.csv"
+
+    # Extract data from external CSV files
+    extracted_data["Airport_lookup"] = pd.read_csv(csv_maintenance_path)
+    extracted_data["Aircraft_lookup"] = pd.read_csv(csv_aircrafts_path)
+
+    return extracted_data
+
+
+def extract_SQL_data(
+    extracted_data: dict[str, pd.DataFrame], conn: psycopg2.extensions.connection
+) -> None:
+    """
+    Pre: extracted_data is a dictionary to store the extracted tables as pandas dataframes
+          connection to DBBDA established
+    Post: extracted_data is updated with the extracted tables (AIMS_flights, AIMS_maint, AMOS)
+           as pandas dataframes"""
+
+    # AIMS queries
+    relevant_flight_cols = [
+        "aircraftregistration",
+        "cancelled",
+        "actualdeparture",
+        "actualarrival",
+        "scheduleddeparture",
+        "scheduledarrival",
+    ]
+    relevant_maint_cols = [
+        "aircraftregistration",
+        "scheduledarrival",
+        "scheduleddeparture",
+        "programmed",
+    ]
+    # AMOS queries
+    relevant_amos_cols = [
+        "aircraftregistration",
+        "executiondate",
+        "reporteurclass",
+        "reporteurid",
+    ]
+
+    extracted_data["AIMS_flights"] = pd.read_sql(
+        f'SELECT {", ".join(relevant_flight_cols)} FROM "AIMS"."flights"', conn
+    )
+    extracted_data["AIMS_maint"] = pd.read_sql(
+        f'SELECT {", ".join(relevant_maint_cols)} FROM "AIMS"."maintenance"', conn
+    )
+    extracted_data["AMOS"] = pd.read_sql(
+        f'SELECT {", ".join(relevant_amos_cols)} FROM "AMOS"."technicallogbookorders"',
+        conn,
+    )
+
+    # debugging
+    for key, df in extracted_data.items():
+        print(f"Extracted {len(df)} rows from {key} table.")
+    return
+
 
 # ====================================================================================================================================
 # Baseline queries
 def get_aircrafts_per_manufacturer() -> dict[str, list[str]]:
-    # TODO: Implement a function to generate a dictionary with one entry per manufacturer and a list of aircraft identifiers as values
+    """Prec:
+    Post: returns a dictionary with one entry per manufacturer and a list of aircraft identifiers as values
+    """
+    aircrafts = {
+        "Airbus": [],
+        "Boeing": [],
+    }
+
+    return aircrafts
 
 
 def query_utilization_baseline():
     aircrafts = get_aircrafts_per_manufacturer()
     cur = conn.cursor()
-    cur.execute(f"""
+    cur.execute(
+        f"""
         WITH atomic_data AS (
             SELECT f.aircraftregistration,
                 CASE 
@@ -121,7 +206,8 @@ def query_utilization_baseline():
         FROM atomic_data a
         GROUP BY a.manufacturer, a.year
         ORDER BY a.manufacturer, a.year;
-        """)
+        """
+    )
     result = cur.fetchall()
     cur.close()
     return result
@@ -130,7 +216,8 @@ def query_utilization_baseline():
 def query_reporting_baseline():
     aircrafts = get_aircrafts_per_manufacturer()
     cur = conn.cursor()
-    cur.execute(f"""
+    cur.execute(
+        f"""
         WITH 
             atomic_data_utilization AS (
                 SELECT
@@ -169,7 +256,8 @@ def query_reporting_baseline():
         FROM atomic_data_reporting f1
             JOIN atomic_data_utilization f2 ON f2.manufacturer = f1.manufacturer AND f1.year = f2.year
         ORDER BY f1.manufacturer, f1.YEAR;
-        """)
+        """
+    )
     result = cur.fetchall()
     cur.close()
     return result
@@ -178,7 +266,8 @@ def query_reporting_baseline():
 def query_reporting_per_role_baseline():
     aircrafts = get_aircrafts_per_manufacturer()
     cur = conn.cursor()
-    cur.execute(f"""
+    cur.execute(
+        f"""
         WITH 
             atomic_data_utilization AS (
                 SELECT
@@ -218,7 +307,23 @@ def query_reporting_per_role_baseline():
         FROM atomic_data_reporting f1
             JOIN atomic_data_utilization f2 ON f2.manufacturer = f1.manufacturer AND f1.year = f2.year
         ORDER BY f1.manufacturer, f1.year, f1.role;
-        """)
+        """
+    )
     result = cur.fetchall()
     cur.close()
     return result
+
+
+# ====================================================================================================================================ç
+#  MAIN
+def main():
+    extracted_data = extract_all(conn)
+    # Example of accessing the extracted dataframes
+    for table_name, df in extracted_data.items():
+        print(f"Table: {table_name}, Number of rows: {len(df)}")
+        print(df.head())  # Print the first few rows of each dataframe
+
+
+if __name__ == "__main__":
+    main()
+    conn.close()
